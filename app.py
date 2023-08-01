a067e003247f7b09d6051b71b9ba0851fe39f15a
ShawnKBeck <48632060+ShawnKBeck@users.noreply.github.com>
2023-07-31 22:47:16 -0500

    adding m4a and split files if longer than ten minutes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from moviepy.editor import *
+from pydub import AudioSegment
 import streamlit as st # Import Streamlit library
 import openai # Import OpenAI API library
 import nltk # Import Natural Language Toolkit
@@ -6,10 +6,24 @@ import os
 
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
+def split_audio(file_path): # Function to split audio file into chunks
+    file_extension = os.path.splitext(file_path)[1]  # Get the file extension
+    if file_extension == '.mp3':
+        song = AudioSegment.from_mp3(file_path)
+    elif file_extension == '.wav':
+        song = AudioSegment.from_wav(file_path)
+    elif file_extension == '.m4a':
+        song = AudioSegment.from_file(file_path, 'm4a')
+    ten_minutes = 10 * 60 * 1000
+    first_10_minutes = song[:ten_minutes]
+    first_10_minutes.export("tempfile.mp3", format="mp3")
+    return "tempfile.mp3"
+
 def transcribe_audio(file_path): # Defining a function to transcribe audio using OpenAI API
-  with open(file_path, "rb") as file: # Opening the audio file in binary mode for transcription
-    transcription = openai.Audio.transcribe("whisper-1", file) # Using OpenAI Audio API to transcribe audio file
-    return transcription["text"] # Returning the transcribed text
+    split_file_path = split_audio(file_path)
+    with open(split_file_path, "rb") as file: # Opening the audio file in binary mode for transcription
+        transcription = openai.Audio.transcribe("whisper-1", file) # Using OpenAI Audio API to transcribe audio file
+        return transcription["text"] # Returning the transcribed text
 
 def summarize_text(text):  # Function to summarize text using OpenAI's GPT-3.5-turbo
     messages = [
@@ -28,42 +42,29 @@ def summarize_text(text):  # Function to summarize text using OpenAI's GPT-3.5-t
 
 st.title('Audio Transcription App')
 
-uploaded_file = st.file_uploader("Upload an audio file", type=['mp3', 'wav'])
+uploaded_file = st.file_uploader("Upload an audio file", type=['mp3', 'wav', 'm4a'])
 
 if uploaded_file is not None:
-  file_extension = os.path.splitext(uploaded_file.name)[1]  # Get the file extension
-  temp_file_path = f"tempfile{file_extension}"  # Create a temporary file name with the same extension
+    file_extension = os.path.splitext(uploaded_file.name)[1]  # Get the file extension
+    temp_file_path = f"tempfile{file_extension}"  # Create a temporary file name with the same extension
 
-  with open(temp_file_path, "wb") as f: 
-    f.write(uploaded_file.getbuffer()) # Writes the uploaded file to a temporary file
+    with open(temp_file_path, "wb") as f: 
+        f.write(uploaded_file.getbuffer()) # Writes the uploaded file to a temporary file
   
-  transcription_text = transcribe_audio(temp_file_path) # Transcribing the audio file
+    transcription_text = transcribe_audio(temp_file_path) # Transcribing the audio file
 
-  # Summarize the transcribed text and break it down into bullet points
-  summary = summarize_text(transcription_text)
+    # Summarize the transcribed text and break it down into bullet points
+    summary = summarize_text(transcription_text)
 
-  nltk.download('punkt') # Downloading pre-trained sentence tokenizer
+    nltk.download('punkt') # Downloading pre-trained sentence tokenizer
 
-  sentences = nltk.tokenize.sent_tokenize(transcription_text) # Tokenizing transcribed text into sentences
+    sentences = nltk.tokenize.sent_tokenize(transcription_text) # Tokenizing transcribed text into sentences
 
-  with open('formatted_transcription.txt', 'w') as f: # Writing sentences to a file
-    for sentence in sentences:
-        f.write(sentence + '\\n')
+    with open('formatted_transcription.txt', 'w') as f: # Writing sentences to a file
+        for sentence in sentences:
+            f.write(sentence + '\\n')
 
-  st.write("Transcription:", transcription_text) # Printing transcription
-  st.write("Summary:", summary)  # Printing the summary
+    st.write("Transcription:", transcription_text) # Printing transcription
+    st.write("Summary:", summary)  # Printing the summary
 
-def convert_mp4_to_mp3(mp4_file):
-    mp3_path = mp4_file.replace('.mp4', '.mp3')
-    video = VideoFileClip(mp4_file)
-    audio = video.audio
-    audio.write_audiofile(mp3_path)
-    return mp3_path
 
-uploaded_file = st.file_uploader("Upload an MP4 file", type="mp4")
-if uploaded_file is not None:
-    mp4_file_path = "uploaded.mp4"
-    with open(mp4_file_path, "wb") as f:
-        f.write(uploaded_file.getbuffer())
-    mp3_file_path = convert_mp4_to_mp3(mp4_file_path)
-    st.write(f"MP3 file created: {mp3_file_path}")
